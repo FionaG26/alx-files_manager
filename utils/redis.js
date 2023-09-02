6931641cbb864b1c29669b726333ca8c556654c0
FionaG26 <fionamuthoni18@gmail.com>
2023-09-02 10:55:41 -0700

    redis

--- utils/redis.js
+++ utils/redis.js
@@ -1,9 +1,5 @@
 import { promisify } from 'util';
 import { createClient } from 'redis';
-import dotenv from 'dotenv';
-
-// Load environment variables from a .env file
-dotenv.config();
 
 /**
  * Represents a Redis client.
