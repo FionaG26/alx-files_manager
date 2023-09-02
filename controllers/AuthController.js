d2210538a0e4229569ec70775d82e17dec87546a
FionaG26 <fionamuthoni18@gmail.com>
2023-09-02 11:49:26 -0700

    authorize

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,42 +1,19 @@
-import sha1 from 'sha1';
 import { v4 as uuidv4 } from 'uuid';
-import DBClient from '../utils/db';
-import RedisClient from '../utils/redis';
+import { redisClient } from '../utils/redis';
 
-class AuthController {
-  static async getConnect(request, response) {
-    const auth = request.header('Authorization') || null;
-    if (!auth) return response.status(401).send({ error: 'Unauthorized' });
-
-    const buff = Buffer.from(auth.replace('Basic ', ''), 'base64');
-    const creds = {
-      email: buff.toString('utf8').split(':')[0],
-      password: buff.toString('utf8').split(':')[1],
-    };
-    creds.password = sha1(creds.password);
-    if (!creds.email || !creds.password) return response.status(401).send({ error: 'Unauthorized' });
-
-    const getUser = await DBClient.db.collection('users').findOne(creds);
-    if (!getUser) return response.status(401).send({ error: 'Unauthorized' });
-
+export default class AuthController {
+  static async getConnect(req, res) {
+    const { user } = req;
     const token = uuidv4();
-    const key = `auth_${token}`;
 
-    await RedisClient.set(key, getUser._id.toString(), 86400);
-    return response.status(200).send({ token });
+    await redisClient.set(`auth_${token}`, user._id.toString(), 24 * 60 * 60);
+    res.status(200).json({ token });
   }
 
-  static async getDisconnect(request, response) {
-    const token = request.header('X-Token') || null;
+  static async getDisconnect(req, res) {
+    const token = req.headers['x-token'];
 
-    if (!token) return response.status(401).send({ error: 'Unauthorized' });
-
-    const userId = RedisClient.get(`auth_${token}`);
-    if (!userId) return response.status(401).send({ error: 'Unauthorized' });
-
-    await RedisClient.del(`auth_${token}`);
-    return response.status(204).send();
+    await redisClient.del(`auth_${token}`);
+    res.status(204).send();
   }
 }
-
-module.exports = AuthController;
